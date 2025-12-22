40b4d2607c8e48319c4efbf13f99c2036086158c
Kiran Kumar H.M <kirankumarhm2004@gmail.com>
2025-12-22 20:29:16 +0530

    Initialize Spring Boot application

---
+++ src/main/java/com/opa/demo/OpaSpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.opa.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpaSpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpaSpringBootDemoApplication.class, args);
+	}
+
+}
